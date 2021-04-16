c4ea24acdaa94000890cd755559a3c5dac4cd2ea
Krim10000 <camilo.rojas@usach.cl>
2021-04-15 20:32:22 -0400

    Update settings.py

--- REPORTE/settings.py
+++ REPORTE/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'uj=(!f)ci8_gn*e*omi**ff7e=zad#3z#m_bv*k+**)4an&3+l'
+SECRET_KEY =...
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
